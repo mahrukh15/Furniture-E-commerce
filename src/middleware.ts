f42e730a284de3f9ce70d962812d78c60da26d0b
mahrukh15 <sajidmahrukh93@gmail.com>
2025-02-09 19:40:24 +0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,36 +1,18 @@
-// // import { getCookie } from 'cookies-next';
-// // import { NextResponse } from 'next/server';
-// // import { NextRequest } from 'next/server';
+import { getCookie } from 'cookies-next';
 
-// // export async function middleware(req: NextRequest) {
-// //     try {
-// //         const accessToken = getCookie('accessToken', { req });
-// //         if (!accessToken) {
-// //             return NextResponse.redirect(new URL('/login', req.url));
-// //         }
-// //     } catch (error) {
-// //         console.log('Error during token verification:', error);
-// //         return NextResponse.redirect(new URL('/login', req.url));
-// //     }
-// // }
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-// // export const config = {
-// //     matcher: [ '/checkout', '/profile'],
-// // };
+const isProtectedRoute = createRouteMatcher(['/profile(.*)', '/checkout(.*)','/sign-in(.*)'])
 
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect()
+})
 
-// const isProtectedRoute = createRouteMatcher(['/profile(.*)', '/checkout(.*)','/sign-in(.*)'])
-
-// export default clerkMiddleware(async (auth, req) => {
-//   if (isProtectedRoute(req)) await auth.protect()
-// })
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API routes
-//     '/(api|trpc)(.*)',
-//   ],
-// }
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
