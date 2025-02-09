c46ee6f32dff1caf8010ba3dcf09129ba6afee6d
tehreem-111 <tehsaj@gmail.com>
2025-02-09 16:02:21 +0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,36 +1,36 @@
-// import { getCookie } from 'cookies-next';
-// import { NextResponse } from 'next/server';
-// import { NextRequest } from 'next/server';
+// // import { getCookie } from 'cookies-next';
+// // import { NextResponse } from 'next/server';
+// // import { NextRequest } from 'next/server';
 
-// export async function middleware(req: NextRequest) {
-//     try {
-//         const accessToken = getCookie('accessToken', { req });
-//         if (!accessToken) {
-//             return NextResponse.redirect(new URL('/login', req.url));
-//         }
-//     } catch (error) {
-//         console.log('Error during token verification:', error);
-//         return NextResponse.redirect(new URL('/login', req.url));
-//     }
-// }
+// // export async function middleware(req: NextRequest) {
+// //     try {
+// //         const accessToken = getCookie('accessToken', { req });
+// //         if (!accessToken) {
+// //             return NextResponse.redirect(new URL('/login', req.url));
+// //         }
+// //     } catch (error) {
+// //         console.log('Error during token verification:', error);
+// //         return NextResponse.redirect(new URL('/login', req.url));
+// //     }
+// // }
+
+// // export const config = {
+// //     matcher: [ '/checkout', '/profile'],
+// // };
+
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+// const isProtectedRoute = createRouteMatcher(['/profile(.*)', '/checkout(.*)','/sign-in(.*)'])
+
+// export default clerkMiddleware(async (auth, req) => {
+//   if (isProtectedRoute(req)) await auth.protect()
+// })
 
 // export const config = {
-//     matcher: [ '/checkout', '/profile'],
-// };
-
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-const isProtectedRoute = createRouteMatcher(['/profile(.*)', '/checkout(.*)','/sign-in(.*)'])
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect()
-})
-
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// }
